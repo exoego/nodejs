c6bf8fe706deffee6650d9938c7403264c781161
exoego <ytatsuno.jp@gmail.com>
2020-08-05 09:48:21 +0900

    Drop Scala.js 0.6

--- script/ci-validate.sh
+++ script/ci-validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCALAJS_VERSION="0.6.33" sbt ++$TRAVIS_SCALA_VERSION test
+sbt ++$TRAVIS_SCALA_VERSION test
 
 if [[ $TRAVIS_SCALA_VERSION = 2.13.* ]]; then
   SCALAJS_VERSION="" sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport;
